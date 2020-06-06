ba18641b2423fbcfcbc84aa4a3e4ae6fd13c0b37
maheenrao <maheenrao@users.noreply.github.com>
2020-06-06 13:43:27 +0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-         <h1>welcome to my first react</h1> 
-          deployed by Maheen Rao
-          Student of PIAIC
+          welcome<code></code> to my first react.
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
